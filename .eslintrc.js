9155da3d082f8df443f83f0fa23e0eeeaba06b06
Huy <khangwhat116@gmail.com>
2022-04-19 15:37:52 +0700

    setup extensions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "@react-native-community",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "no-console": "warn",
+    "react/prop-types": 0,
+    "linebreak-style": ["error", "unix"],
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+}
