36dcf849172ec3e8019b580af312558a2196e1c7
Huy <khangwhat116@gmail.com>
2022-04-22 12:12:31 +0700

    added setup firebase with react native

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,19 @@
+import {View, Text} from "react-native"
+import React, {useEffect} from "react"
+import auth from "@react-native-firebase/auth"
+import {useState} from "react"
+
+const useAuth = () => {
+  const [userInfo, setUserInfo] = useState(null)
+  function onAuthStateChanged(user) {
+    setUserInfo(user)
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
+    return subscriber // unsubscribe on unmount
+  }, [])
+  return {userInfo}
+}
+
+export default useAuth
