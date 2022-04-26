d99922efd6decf926d47106f055f941472981deb
Huy <khangwhat116@gmail.com>
2022-04-26 13:40:34 +0700

    fix onpress fb

--- src/screens/auth/components/register/socialContainer.js
+++ src/screens/auth/components/register/socialContainer.js
@@ -62,7 +62,7 @@ const SocialContainer = () => {
   return (
     <View style={styles.container}>
       <SocicalButton
-        onPress={() => onFacebookButtonPress}
+        onPress={onFacebookButtonPress}
         uri={
           "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/800px-Facebook_f_logo_%282019%29.svg.png"
         }
